57bf0cb1d19d3705d10b22d0b45138ad766ea9ae
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-23 10:15:23 +0100

    Add project files.

---
+++ Statistika_IT1B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Statistika_IT1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
